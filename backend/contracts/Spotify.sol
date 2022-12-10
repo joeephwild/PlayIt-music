f17cf1bf5365d57709822d5f06ac6e1c3698d559
joeephwild <josephomotade0@gmail.com>
2022-12-10 03:15:47 -0800

    commit changes to smart contract

--- backend/contracts/Spotify.sol
+++ backend/contracts/Spotify.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
+ /**
+  *  
+    //two seperate segment listener and artist if(user == artsist){//run}
+    //make an every month payment using solidity
+  * list of unction needed in the dapp  
+  * Create and manage user accounts
+    Allow users to upload and share music tracks
+    Set prices for tracks and manage payments
+    Track playback and listening statistics
+  */
 contract Spotify {
     uint256 musicCount;
     uint256 followerCount;
-    uint registrationFee = 0.02 ethers;
-    //two seperate segment listener and artist if(user == artsist){//run}
-    //make an every month payment using solidity
+    uint registrationFee;
 
     struct Music {
         string title;
@@ -16,7 +23,7 @@ contract Spotify {
 
     struct ArtistStruct {
         string name;
-        string description;
+        string image;
         address artist;
         uint followerCount;
         uint music;
@@ -25,6 +32,17 @@ contract Spotify {
     struct userStruct {
         string name;
         address user;
+    }
+
+ //holds the different account type a user 
+ //can create an account has which will be used later for account creation
+    enum AccountType {
+        Artist,
+        Listener
+    }
+
+  //constructor to hold the fee a user has to pay
+    constructor() public {
     }
 
     //an array of artist
@@ -69,4 +87,12 @@ contract Spotify {
     function createAnAccount() public {
         require(registrationFee, "fee has to be have been payed");
     }
+
+      // Define a mapping to store the price of each track
+     mapping (bytes32 => uint) public trackPrices;
+
+  // Define a function to allow the artist to set the price for their track
+  function setTrackPrice(bytes32 trackId, uint price) public {
+    trackPrices[trackId] = price;
+  }
 }
